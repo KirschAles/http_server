6cb0244a173771c7ac86fb0c9da13ae11eac9d9b
Ales Kirsch <kirschales@gmail.com>
2022-04-13 11:50:29 +0200

    Created Basic SetUp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-//
-// Created by alesp on 11.04.2022.
-//
-
+#include <sys/socket.h>
+#include <iostream>
 int main() {
+    int sck = socket(AF_INET, SOCK_STREAM, 0);
+    std::cout << sck << std::endl;
     return 0;
 }
