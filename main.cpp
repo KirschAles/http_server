4a83416b9c0d66f5a0e5f6be47e977192e98341e
Ales Kirsch <kirschales@gmail.com>
2022-05-02 13:09:14 +0200

    Created class for Requests

--- main.cpp
+++ main.cpp
@@ -101,6 +101,106 @@ namespace http {
             return std::move(line);
         }
     };
+    class Request {
+    protected:
+        std::string fileName;
+        std::string httpVersion;
+        std::map<std::string, std::string> headers;
+
+        std::string setFileName(const std::string line) {
+            size_t pos;
+            for (pos = 0; line[pos] != ' '; pos++) {
+                fileName += line[pos];
+                // put here so if there is no ' ' in string,
+                // the posisition doesnt go out of bonds
+                if (pos == line.length() - 1) {
+                    break;
+                }
+            }
+            return std::move(line.substr(++pos));
+        }
+        void setHttpVersion(const std::string &line) {
+            // there should be nothing else but the http version here
+            httpVersion = line;
+            // if simple request is used and thus there is no http version
+            // the line should be empty string
+        }
+        void setRequestLine(Connection &connection) {
+            std::string line = connection.getLine();
+            std::string remainingLine = setFileName(line);
+            setHttpVersion(remainingLine);
+
+        }
+        // line must be at least one character long, otherwise undefined behaviour
+        std::string getHeaderName(std::string &line) {
+            std::string name;
+            char tmp;
+            size_t pos;
+            for (pos = 0; ':' != (tmp = line[pos]); pos++) {
+                // attribute name must be made of printable characters
+                if (!std::isprint(tmp)) {
+                    throw std::runtime_error("Header name in request is made of non-printable characters.");
+                }
+                else if (std::isspace(tmp)) {
+                    throw std::runtime_error("Header name in request contains whitespace characters");
+                }
+                // the header name must not depend on the case of character
+                name += tolower(tmp);
+                if (pos == line.length() - 1) {
+                    throw std::runtime_error("No ':' character in header");
+                }
+            }
+            if (pos >= line.length() - 2) {
+                throw std::runtime_error("No value in header");
+            }
+            if (line[++pos] != ' ') {
+                throw std::runtime_error("Bad format of header, missing space after ':'");
+            }
+            line = line.substr(++pos);
+            return std::move(name);
+        }
+        void setHeader(std::string &line) {
+            std::string name = getHeaderName(line);
+            for (size_t pos = 0; pos < line.length(); pos++) {
+                headers[name] += line[pos];
+            }
+        }
+        bool setHeaders(Connection &connection) {
+            std::string line = connection.getLine();
+            while (line.length() != 0) {
+                setHeader(line);
+                line = connection.getLine();
+            }
+            return true;
+        }
+    public:
+        Request() {}
+        // On ERROR throws std::runtime_error
+        Request(Connection &connection) {
+            setRequestLine(connection);
+            setHeaders(connection);
+        }
+        
+
+
+
+        void print() {
+            std::cout << fileName << std::endl << httpVersion << std::endl;
+            for (auto i: headers) {
+                std::cout << i.first << ": " << i.second << std::endl;
+            }
+        }
+
+    };
+    class GETRequest: public Request {
+    private:
+    public:
+        GETRequest(Connection &connection)
+        : Request(connection) {
+
+        }
+    };
+
 }
 
 int main() {
