76edf7489d9c85579a0aded6c9dbc11e704886ac
Ales Kirsch <kirschales@gmail.com>
2022-06-04 01:44:56 +0200

    Implemented sendHeaders()

--- loggable/responses/FullResponse.cpp
+++ loggable/responses/FullResponse.cpp
@@ -3,8 +3,15 @@ bool FullResponse::sendStatusLine() {
     std::string line = httpVersion + " " + codes::OK + " " + "ok" + http::CRLF;
     return connection.send(line);
 }
+// TO DO: need to also implement sending of general headers, not only entity ones
 bool FullResponse::sendHeaders(contentGenerator &contentGenerator) {
-    return true;
+    std::string headers;
+    for (auto &headerValuePair: contentGenerator.getHeaders()) {
+        headers += headerValuePair.first + ": " + headerValuePair.second + http::CRLF;
+    }
+    headers += http::CRLF;
+    headers += http::CRLF;
+    return connection.send(headers);
 }
 bool FullResponse::send(contentGenerator &contentGenerator) {
     return sendStatusLine() && sendHeaders(contentGenerator) && sendBody(contentGenerator);
