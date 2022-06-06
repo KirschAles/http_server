f68f393899ec68afdc50b2bc49d57c4ef8e3c52a
Ales Kirsch <kirschales@gmail.com>
2022-06-06 12:39:19 +0200

    Dealt with multiple definitions error

--- constants/codes.h
+++ constants/codes.h
@@ -1,33 +1,35 @@
 #ifndef HTTP_SERVER_CODES_H
 #define HTTP_SERVER_CODES_H
-namespace codes {
-    const char *OK                          = "200";
-    const char *Created                     = "201";
-    const char *Accepted                    = "202";
-    const char *NonAuthoritativeInformation = "203";
-    const char *NoContent                   = "204";
-    const char *MultipleChoices             = "300";
-    const char *MovedPermanently            = "301";
-    const char *MovedTemporarily            = "302";
-    const char *SeeOther                    = "303";
-    const char *NotModified                 = "304";
-    const char *BadRequest                  = "400";
-    const char *Unauthorized                = "401";
-    const char *PaymentRequired             = "402";
-    const char *Forbidden                   = "403";
-    const char *NotFound                    = "404";
-    const char *MethodNotAllowed            = "405";
-    const char *NoneAcceptable              = "406";
-    const char *ProxyAuthenticationRequired = "407";
-    const char *RequestTimeout              = "408";
-    const char *Conflict                    = "409";
-    const char *Gone                        = "410";
-    const char *AuthorizationRefused        = "411";
-    const char *InternalServerError         = "500";
-    const char *NotImplemented              = "501";
-    const char *BadGateway                  = "502";
-    const char *ServiceUnavailable          = "503";
-    const char *GatewayTimeout              = "504";
 
+#include <string>
+namespace codes {
+    const std::string OK = "200";
+    const std::string Created = "201";
+    const std::string Accepted = "202";
+    const std::string NonAuthoritativeInformation = "203";
+    const std::string NoContent = "204";
+    const std::string MultipleChoices = "300";
+    const std::string MovedPermanently = "301";
+    const std::string MovedTemporarily = "302";
+    const std::string SeeOther = "303";
+    const std::string NotModified = "304";
+    const std::string BadRequest = "400";
+    const std::string Unauthorized = "401";
+    const std::string PaymentRequired = "402";
+    const std::string Forbidden = "403";
+    const std::string NotFound = "404";
+    const std::string MethodNotAllowed = "405";
+    const std::string NoneAcceptable = "406";
+    const std::string ProxyAuthenticationRequired = "407";
+    const std::string RequestTimeout = "408";
+    const std::string Conflict = "409";
+    const std::string Gone = "410";
+    const std::string AuthorizationRefused = "411";
+    const std::string InternalServerError = "500";
+    const std::string NotImplemented = "501";
+    const std::string BadGateway = "502";
+    const std::string ServiceUnavailable = "503";
+    const std::string GatewayTimeout = "504";
 }
+
 #endif //HTTP_SERVER_CODES_H
