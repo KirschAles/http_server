5cd01b3b63f57a0b06604cf1543275a688529ff4
Ales Kirsch <kirschales@gmail.com>
2022-06-08 23:18:11 +0200

    Added constant Unknown

---
+++ constants/mixed.h
@@ -0,0 +1,9 @@
+#ifndef HTTP_SERVER_MIXED_H
+#define HTTP_SERVER_MIXED_H
+#include <string>
+
+namespace mixed {
+    // Used as placeholder value for when the information couldn't be reached
+    const std::string unknown = "Unknown.";
+}
+#endif //HTTP_SERVER_MIXED_H
