bf38b52ca18cde7458bce961d33af4a883615687
Ales Kirsch <kirschales@gmail.com>
2022-06-08 21:59:04 +0200

    Added accepted log format strings

---
+++ constants/format.h
@@ -0,0 +1,17 @@
+#ifndef HTTP_SERVER_FORMAT_H
+#define HTTP_SERVER_FORMAT_H
+#include <string>
+#include <vector>
+namespace format {
+    const std::string formatter = "%";
+    const std::string message = formatter + "m";
+    const std::string time = formatter + "t";
+    const std::string ipAddress = formatter + "I";
+    const std::string port = formatter + "p";
+    const std::string domain = formatter + "d";
+    const std::string procent = formatter + formatter;
+
+    std::vector<std::string> allFormats {message, time, ipAddress, port, domain, procent};
+}
+
+#endif //HTTP_SERVER_FORMAT_H
