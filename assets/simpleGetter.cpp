b661b0526e463d453c80cb082d07e28afe3bd965
Ales Kirsch <kirschales@gmail.com>
2022-06-09 12:24:37 +0200

    Updated simpleGetter

--- assets/simpleGetter.cpp
+++ assets/simpleGetter.cpp
@@ -7,7 +7,8 @@
 #include <unistd.h>
 
 int PORT = 8080;
-const std::string CRLF = "/r/n";
+const std::string CRLF = "\r\n";
+const std::string LF="\n\n";
 int main (){
     struct addrinfo hints, *result;
     memset(&hints, 0, sizeof hints);
@@ -19,33 +20,24 @@ int main (){
         throw std::exception();
     }
     int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
+    if (-1 == connect(sockfd, result->ai_addr, result->ai_addrlen)) {
+        std::cout << "fail" << std::endl;
+        return -1;
+    }
     while (true) {
-        if (-1 == connect(sockfd, result->ai_addr, result->ai_addrlen)) {
-            std::cout << "fail" << std::endl;
-            return -1;
-        }
+
         std::cout << "Connected" << std::endl;
 
         std::string message;
         getline(std::cin, message);
-        std::cout << message << std::endl;
-        char *sending = new char[message.length() + 4];
-        int i = 0;
-        for (; i < message.length(); i++){
-            sending[i] = message[i];
-        }
-        sending[++i] = CRLF[0];
-        sending[++i] = CRLF[1];
-        sending[++i] = CRLF[0];
-        sending[++i] = CRLF[1];
-        int originalLength = message.length() + 4;
-        int length = message.length() + 4;
+        std::cout << message;
+        message += CRLF + CRLF;
+        int originalLength = message.length();
+        int length = message.length();
         int bytesSent = 0;
-        std::cout << length << std::endl;
+
         while (length -= bytesSent) {
-            bytesSent = send(sockfd, (void *) (sending + originalLength - length), length, 0);
-            std::cout << bytesSent << "   " << length << std::endl;
+            bytesSent = send(sockfd, (void *) (message.c_str() + originalLength - length), length, 0);
         }
         char recieved[100];
         recieved[recv(sockfd, (void *) recieved, 100, 0)] = '\0';
